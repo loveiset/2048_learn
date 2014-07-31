690cd58402cd6c288e7b850dbb69ffd813f05742
loveiset <loveiset@qq.com>
2014-07-31 19:19:34 +0800

    1
    1

---
+++ Assets/Scripts/helpPanel/HelpButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpButton : MonoBehaviour {
+
+
+    public void OnClick(dfControl control, dfMouseEventArgs args)
+    {
+        HelpPanelCloseButton._instance.BounceIn();
+    }
+}
---
+++ Assets/Scripts/helpPanel/HelpPanelCloseButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpPanelCloseButton : MonoBehaviour {
+    public static HelpPanelCloseButton _instance;
+    public dfTweenVector3 tween;
+
+    public void Awake()
+    {
+        _instance = this;
+    }
+
+    public void BounceIn()
+    {
+        tween.EndValue = new Vector3(0, 0, 0);
+        tween.Play();
+    }
+
+    public void BounceOut()
+    {
+        tween.EndValue = new Vector3(Screen.width, 0, 0);
+        tween.Play();
+    }
+
+    public void OnClick(dfControl control,dfMouseEventArgs args)
+    {
+        BounceOut();
+    }
+}
